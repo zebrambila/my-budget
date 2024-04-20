674ecbc3644f3a8167465e6ac72654be6f0fbfd3
Jose Eduardo Brambila <jose.e.brambila@gmail.com>
2024-04-20 14:56:30 -0300

    Ajustando Titulo Projeto

--- components/sidebar.py
+++ components/sidebar.py
@@ -25,8 +25,8 @@ from globals import *
 
 # ========= Layout ========= #
 layout = dbc.Card([
-                html.H1("MyBudget", className="text-primary"),
-                html.P("By ASIMOV", className="text-info"),
+                html.H1("Meu Projeto Receitas e Despesas", className="text-primary"),
+                #html.P("By ASIMOV", className="text-info"),
                 html.Hr(),
 
 
